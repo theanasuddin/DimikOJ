57d3672c167af82c70a8d87e57b150cb552a1320
Anas <anasuddin44@gmail.com>
2020-11-26 14:54:04 +0600

    বাংলাদেশ (ISCPC)
    বাংলাদেশ (ISCPC)
    ==============
    আমরা সবাই আমাদের দেশকে ভালোবাসি। বাংলাদেশকে ভালোবাসি কথাটা আমরা আমাদের মুখে প্রায় সময় বলে থাকি, কিন্তু আমরা কম্পিউটারের ভাষায় " আমি বাংলাদেশকে ভালোবাসি" কথাটি বলব।
    
    তোমাকে এমন একটি প্রোগ্রাম লিখতে হবে যেটি স্ক্রিনে নিচের ৩টি লাইন প্রিন্ট করে।
    
    I
    Love
    Bangladesh.
    
    স্যাম্পল ইনপুট আউটপুট
    ==================
    স্যাম্পল #1
    
    ইনপুট
    This problem doesn't have any input.
    
    আউটপুট
    I
    Love
    Bangladesh.

---
+++ Problem70.java
@@ -0,0 +1,15 @@
+package dimikOJ;
+
+public class Problem70 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message = "I Love Bangladesh.";
+		String[] tempArray = message.split("\\s");
+		for (String string : tempArray) {
+			System.out.println(string);
+		}
+
+	}
+
+}
