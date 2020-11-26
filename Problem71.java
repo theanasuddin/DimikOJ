7adbffd71ffbfeb66e3b9e52eb57419b27265d7f
Anas <anasuddin44@gmail.com>
2020-11-26 14:49:23 +0600

    লুকোচুরি খেলা (ISCPC 2018 Preliminary)
    লুকোচুরি খেলা (ISCPC 2018 Preliminary)
    ===============================
    লুকোচুরি খেলায় একজনকে চোখ বন্ধ করে ধীরে ধীরে দশ থেকে এক পর্যন্ত গুনতে হয়। এসময় অন্যরা কোথাও না কোথাও লুকিয়ে যায়। তোমার কাজ হচ্ছে একটা প্রোগ্রাম লেখা যেন সেটি দশ থেকে এক পর্যন্ত সবগুলো সংখ্যা প্রিন্ট করে।
    
    Input
    এই প্রোগ্রামে কোনো ইনপুট নেই।
    
    Output
    আউটপুটে মোট দশটি লাইন থাকবে, নিচের নমুনা আউটপুটের মতো।
    
    স্যাম্পল ইনপুট আউটপুট
    ==================
    স্যাম্পল #1
    
    ইনপুট
    This problem doesn't have any input.
    
    আউটপুট
    10
    9
    8
    7
    6
    5
    4
    3
    2
    1

---
+++ Problem71.java
@@ -0,0 +1,13 @@
+package dimikOJ;
+
+public class Problem71 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
